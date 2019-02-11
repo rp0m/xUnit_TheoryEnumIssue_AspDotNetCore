928148dab6d813686d3f61d158942bcb450d5449
Ross <38060547+rp0m@users.noreply.github.com>
2019-02-11 15:25:35 +1100

    Initial commit with broken xUnit unit tests

---
+++ xUnit_Test.Test/NumbersTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+using xUnit_Test.Enums;
+
+namespace xUnit_Test.Test
+{
+    public class NumbersTest
+    {
+        [Fact]
+        public void Fact()
+        {
+            var enumVal = Numbers.Two;
+            Assert.Equal(Numbers.Two, enumVal);
+        }
+
+        [Theory]
+        [InlineData((int)Numbers.Two)]
+        public void IntTheory(int value)
+        {
+            Assert.Equal(Numbers.Two, (Numbers)value);
+        }
+
+        [Theory]
+        [InlineData(Numbers.Two)]
+        public void EnumTheory(Numbers value)
+        {
+            Assert.Equal(Numbers.Two, value);
+        }
+
+        [Theory]
+        [InlineData(Numbers.Two, 2)]
+        public void EnumTheory2(Numbers value, int value2)
+        {
+            Assert.Equal((int)value, value2);
+        }
+
+        [Theory]
+        [InlineData(Numbers.Zero, 0)]
+        [InlineData(Numbers.One, 1)]
+        [InlineData(Numbers.Two, 2)]
+        public void EnumTheory3(Numbers value, int value2)
+        {
+            Assert.Equal((int)value, value2);
+        }
+    }
+}
---
+++ xUnit_Test/Enums/Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit_Test.Enums
+{
+    public enum Numbers
+    {
+        Zero,
+        One,
+        Two
+    }
+}
